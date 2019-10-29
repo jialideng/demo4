df5596db13f4469c363d9372deda8d276bf119c4
jialideng <1027392176@qq.com>
2019-10-29 18:03:54 +0800

    添加项目

---
+++ src/main/java/com/example/demo/Demo4Application.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class Demo4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo4Application.class, args);
+	}
+
+	@GetMapping("/domo")
+	public String test(){
+		return "Hello Dengjiali";
+	}
+}
